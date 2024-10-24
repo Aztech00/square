abe039096410cb3860a47bd1cada4f7abc54d03a
Aztech00 <al.harris.2024@nongrad.smu.edu.sg>
2024-10-24 14:50:26 +0800

    Update 4_rectangles.py
    Edited Formatting of the rectangle

--- 4_rectangles.py
+++ 4_rectangles.py
@@ -1,12 +1,12 @@
-def print_square1(n):
+def print_rectangle(n):
     for i in range(n):
         if i == 0 or i == n - 1:
-            print("*" * n)
+            print("*" * int(n + 1))
         elif i == int(n/2):
-            print("*" * n)
+            print("*" * int(n + 1))
         else:
             print("*" + (" " * int(n/2 - 1)) + "*" + (" " * int(n/2 - 1)) + "*")
 
-square_size = int(input("Input a side length"))
+square_size = int(input("Input a side length: "))
 
-print_square1(square_size)
+print_rectangle(square_size)
